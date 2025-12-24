17ab1a248165ac9c8ef001c5073d03f424fe6118
CassioLSalvador <cassiolsalvador@gmail.com>
2025-12-24 16:40:08 -0300

    Added basic 'Hardcoded interest' inclusion

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
-use iced::Theme;
+use std::fmt::format;
+
+use iced::{Fill, Font, Theme, window};
 use iced::font::{Family, Weight, Stretch, Style};
-use iced::{Fill, Font};
-use iced::window;
 use iced::widget::{
   button,
   column,
-  Column,
   container,
+  Container,
+  row,
+  Row,
   text,
 };
 
+#[derive(Clone)]
 struct BulletPoint {
   title: String,
   description: Option<String>,
 }
 
+#[derive(Clone)]
 struct Interest {
   title: String,
   bullet_points: Vec<BulletPoint>,
@@ -32,6 +36,7 @@ struct State {
 enum Message {
   Increment,
   Decrement,
+  AddToList,
 }
 
 impl State {
@@ -43,10 +48,21 @@ impl State {
       Message::Decrement => {
         self.value -= 1;
       }
+      Message::AddToList => {
+        self.interests = Some(vec![Interest {
+          title: String::from("Title 1"),
+          bullet_points: vec![
+            BulletPoint { title: String::from("BP title 1"), description: Some(String::from("BP description 1")) },
+            BulletPoint { title: String::from("BP title 2"), description: Some(String::from("BP description 2")) }
+          ],
+          image: None,
+          interests: None,
+        }]);
+      }
     }
   }
 
-  fn view(&self) -> Column<'_, Message> {
+  fn view(&self) -> Container<'_, Message> {
     let font_definitions = Font {
       family: Family::Fantasy,
       weight: Weight::Bold,
@@ -55,17 +71,46 @@ impl State {
       // ..Default::default()
     };
 
-    column![
-      text("Manage your interests")
-        .font(font_definitions)
-        .size(42)
-        .width(Fill)
-        .height(150)
-        .center(),
-      // button("+").on_press(Message::Increment),
-      // text(self.value),
-      // button("-").on_press(Message::Decrement),
-    ]
+    let mut interests_list = String::new();
+
+    match &self.interests {
+      Some(vector) => {
+        for (index , value) in vector.iter().enumerate() {
+          if index == 0 {
+            interests_list = format!("{}", value.title);
+          } else {
+            interests_list = format!("{} {}", &interests_list, value.title);
+          }
+        }
+      }
+      None => {
+        interests_list = String::from("No interests available, so far...");
+      }
+    };
+
+    container(
+      column![
+        row![
+          text("Manage your interests")
+            .font(font_definitions)
+            .size(42)
+            .width(Fill)
+            .height(75)
+            .center(),
+        ].width(Fill),
+        row![
+          container(
+            button("Add interest").on_press(Message::AddToList)
+          ).align_right(Fill)   
+        ].width(Fill).padding(20),
+        row![
+          text(interests_list)
+            .font(font_definitions)
+            .size(22)
+            .width(Fill)
+        ].padding(20),
+      ]
+    ).width(Fill)
   }
 }
 
